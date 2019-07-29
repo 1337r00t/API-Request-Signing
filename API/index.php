9a52e4703df082db9cd56e740368ec91394f1385
1337r00t <u9cc85@gmail.com>
2019-07-29 19:53:51 +0300

    Update index.php

--- API/index.php
+++ API/index.php
@@ -5,7 +5,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 	$plain_params = $_SERVER['HTTP_PARAMS']; // 'Params' Request Header
 	$signed_params_from_api = base64_encode(hash_hmac('sha1',$plain_params,'123456',true));
 	if($signed_params_from_api == $signed_params_from_app){
-	    echo json_encode(array('output'=>'Correct Request :)'));
+		echo json_encode(array('output'=>'Correct Request :)'));
 	}else{
 		echo json_encode(array('output'=>'Incorrect Request :('));
 		exit(http_response_code(401));
